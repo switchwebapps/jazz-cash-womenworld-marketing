e92d3859f639047d10d924ee8537e982dc52acd7
TayyabNazir611 <tayyabnazir611@gmail.com>
2026-08-05 18:01:14 +0500

    updated tiktok pixel

--- src/app/jazzcash-s-tiktok/page.tsx
+++ src/app/jazzcash-s-tiktok/page.tsx
@@ -23,8 +23,16 @@ const BRAND = {
   purple: "#6B2D85",
 };
 
+const TiktokScript = `!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
 
-const META_PIXEL_ID = "1000950782856119";
+
+  ttq.load('D9PJ1BBC77UFAJG525P0');
+  ttq.page();
+}(window, document, 'ttq');`;
+
 function JazzCashSuccessContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -39,17 +47,18 @@ function JazzCashSuccessContent() {
     setModalOpen(hasSuccessParam);
   }, [hasSuccessParam]);
 
-  const metaPixelScript = `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '${META_PIXEL_ID}');
-fbq('track', 'PageView');
-${isConversion ? "fbq('track', 'Subscribe', { value: 1, currency: 'PKR' });" : ""}`;
+  useEffect(() => {
+    if (isConversion) {
+      const script = document.createElement("script");
+      script.innerHTML = TiktokScript;
+      document.head.appendChild(script);
+      console.log("TiktokScript subscribe event added ");
+
+      // @ts-expect-error
+      window?.ttq?.track("Subscribe");
+
+    }
+  }, [isConversion]);
 
   const handleContinueClick = () => {
     const ua = window.navigator.userAgent;
@@ -60,10 +69,6 @@ ${isConversion ? "fbq('track', 'Subscribe', { value: 1, currency: 'PKR' });" : "
 
   return (
     <div className={`${poppins.className} min-h-screen w-full`}>
-      <Script id="meta-pixel" strategy="afterInteractive">
-        {metaPixelScript}
-      </Script>
-
       <div className={modalOpen ? "pointer-events-none blur-[6px]" : ""}>
         <JazzCashLanding />
       </div>
